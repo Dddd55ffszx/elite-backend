ff3d767f46f90edcec2b2de493b15441d7ef21b3
Dddd55ffszx <danielmorkos2005@gmail.com>
2026-08-26 11:55:53 +0300

    adding balance

--- routes/Balance.routes.js
+++ routes/Balance.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-const { getBalance, addBalance } = require("../controllers/balancController");
+const { getBalance, addBalance } = require("../controllers/balanceController");
 
 router.get("/", auth, getBalance);
 router.post("/", auth, addBalance);
