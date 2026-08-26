e609d069fc25c6c10450f234626b5b4366255337
Dddd55ffszx <danielmorkos2005@gmail.com>
2026-08-26 11:24:48 +0300

    adding balance

---
+++ models/Balance.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const BalanceSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Balance", BalanceSchema);
